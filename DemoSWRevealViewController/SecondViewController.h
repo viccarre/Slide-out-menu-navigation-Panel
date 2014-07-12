9be6cd512adbf036ac2c60476883969f561dced5
Victor Carreño <viccarre@victors-mbp.gateway.2wire.net>
2014-07-12 15:00:35 -0500

    Updated

---
+++ DemoSWRevealViewController/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  DemoSWRevealViewController
+//
+//  Created by Victor Carreño on 7/12/14.
+//  Copyright (c) 2014 viccarre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *revealButtonItem;
+
+@end
--- DemoSWRevealViewController/SecondViewController.m
+++ DemoSWRevealViewController/SecondViewController.m
@@ -1,19 +1,19 @@
 //
-//  VistaDosViewController.m
+//  SecondViewController.m
 //  DemoSWRevealViewController
 //
-//  Created by @viccarre on 11/11/13.
-//  Copyright (c) 2013 viccarre. All rights reserved.
+//  Created by Victor Carreño on 7/12/14.
+//  Copyright (c) 2014 viccarre. All rights reserved.
 //
 
-#import "VistaDosViewController.h"
+#import "SecondViewController.h"
 #import "SWRevealViewController.h"
 
-@interface VistaDosViewController ()
+@interface SecondViewController ()
 
 @end
 
-@implementation VistaDosViewController
+@implementation SecondViewController
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -27,10 +27,13 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	[self.revealButtonItem setTarget: self.revealViewController];
+   
+    [super viewDidLoad];
+    [self.revealButtonItem setTarget: self.revealViewController];
     [self.revealButtonItem setAction: @selector( revealToggle: )];
     [self.navigationController.navigationBar addGestureRecognizer: self.revealViewController.
      panGestureRecognizer];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -38,5 +41,6 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 @end
